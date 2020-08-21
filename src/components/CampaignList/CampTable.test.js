194d8d3bc2eb70274de08f8f151cb93062063891
Tushar Shyoran <tushar.shyoran@nagarro.com>
2020-08-21 19:58:26 +0530

    file name fix

--- src/components/CampaignList/CampTable.test.js
+++ src/components/CampaignList/CampTable.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import CampaignTable from './Camptable';
-import { shallow } from 'enzyme';
-import { Provider } from 'react-redux';
-import store from '../../redux/store';
+import React from "react";
+import CampaignTable from "./CampTable";
+import { shallow } from "enzyme";
+import { Provider } from "react-redux";
+import store from "../../redux/store";
 
-describe('Campaign List', () => {
-  it('should render Campaign List correctly', () => {
+describe("Campaign List", () => {
+  it("should render Campaign List correctly", () => {
     const component = shallow(
       <Provider store={store}>
         <CampaignTable />
